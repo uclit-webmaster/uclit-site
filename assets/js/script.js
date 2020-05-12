6dfba94f2f2a78cfd35c553f6908de0dc7beb3ea
edgar <gesarki@gmail.com>
2020-05-12 04:52:49 -0400

    tweaked background, added The Council page

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Change the navbar color on scroll
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
